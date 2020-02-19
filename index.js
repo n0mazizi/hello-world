8dced7e670dfcf79d1853066801c103ea0792302
n0mazizi <heoyunhoe@gmail.com>
2020-02-19 16:40:26 +0900

    second

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 var express=require('express');//설치한 express module을 불러와서 변수(express)에 담음.
 var app=express();  //express를 실행하여 app object를 초기화
 
-app.get('/', function(req, res){  // '/' 위치에 'get' 요청을 받은 경우,
-  res.send('TEST git Express');   // 이 문구를 보냄.
-});
-
+app.use(express.static(__dirname + '/public'));
 
 var port=3000; //사용할 포트 번호 port 변수
 
